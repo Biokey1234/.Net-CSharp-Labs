39d3f617bf957eb2bf533ddcccb39001715b3737
Biokey1234 <156972714+Biokey1234@users.noreply.github.com>
2025-02-11 22:55:39 -0500

    lab4 completed

---
+++ Lab4/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Lab4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing(int monkeys)
+        {
+            if (monkeys < 20 || monkeys > 30)
+            {
+                return RedirectToAction("SongForm");
+            }
+
+            ViewData["MonkeyCount"] = monkeys;
+            return View();
+        }
+
+        public IActionResult CreateStudent() => View();
+        
+        [HttpPost]
+        public IActionResult DisplayStudent(Student student)
+        {
+            return View(student); 
+        }
+        
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Lab4/Models/Student.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab4.Models
+{
+    public class Student
+    {
+
+        public required string FirstName
+        {
+            get; set;
+        }
+
+        public required string LastName
+        {
+            get; set;
+        }
+
+        public required int StudentId
+        {
+            get; set;
+        }
+
+        public required string EmailAddress
+        {
+            get; set;
+        }
+
+        public required string Password
+        {
+
+            get; set;
+        }
+
+        public required string Description
+        {
+            get; set;
+        }
+    }
+
+
+}
